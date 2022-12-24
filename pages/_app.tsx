ac3473c44cf5981ee523ace2110f9e1005e719c4
hoolek77 <szymon.kin@gmail.com>
2022-12-24 18:19:05 +0100

    config: Setup eslint and remove boilerplate code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
